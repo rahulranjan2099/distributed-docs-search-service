a662820096432d6e3d354afccef9e450b30c023b
rahulranjan2099 <rahulranjantom@gmail.com>
2026-04-25 21:21:40 +0530

    fixed docker sequelize error

--- api-service/config/config.js
+++ api-service/config/config.js
@@ -2,13 +2,16 @@ require("dotenv").config({
   path: process.env.NODE_ENV === "docker" ? ".env.docker" : ".env",
 });
 
+const postgresConfig = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+};
+
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-  },
+  development: postgresConfig,
+  docker: postgresConfig,
 };
